fe390ee9d3fa74bdd8369ec621da0095151dceee
bigegi84 <gilangpw84@gmail.com>
2022-12-13 06:50:45 +0000

    up

--- webapi-jwt/Program.cs
+++ webapi-jwt/Program.cs
@@ -6,11 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication(x =>
@@ -55,35 +51,17 @@ builder.Services.AddSwaggerGen(c =>
        });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
 app.MapControllers();
-
-// app.MapGet("/secret", (ClaimsPrincipal user, [FromHeader(Name = "Authorization")] string header) =>
-// {
-//     Console.WriteLine(user);
-//     return $"Hello {user.Identity?.Name}. My secret";
-// })
-// .RequireAuthorization();
-
 app.UseCors(x => x
     .AllowAnyOrigin()
     .AllowAnyMethod()
     .AllowAnyHeader());
-
-// custom jwt auth middleware
 app.UseMiddleware<JwtMiddleware>();
-
 app.UseAuthorization();
-
 app.Run();
